f5b8485d679bf14fbd37f9021665ae24b54c61aa
Jouko Johansson <joukojo@gmail.com>
2011-11-22 16:39:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<? include_once 'includes/page-start.php' ?>
+
+<h1>Main Content Column</h1><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p> <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+
+<? include_once 'includes/page-end.php' ?>
+<script>
+            var $result; 
+            $(document).ready(function() {
+                console.log('ajax request');
+                viewImageFolders();
+            });
+            
+            console.log('done');
+        </script>
+
+    </body>
+</html>
