41a19ffd789df108c06fb9b9d7c5db5119e3a494
Jouko Johansson <joukojo@gmail.com>
2011-12-08 22:44:16 +0200

    fixed layout on success postings

---
+++ admin/admin-folder.php
@@ -0,0 +1,22 @@
+<? ?>
+
+<? include_once '../includes/admin-page-start.php' ?>
+
+<h1>View a folder</h1>
+
+
+
+
+
+
+<? include_once '../includes/admin-page-end.php' ?>
+
+<script>
+    var $result; 
+    $(document).ready(function() {
+        
+    });
+</script>
+
+</body>
+</html>
